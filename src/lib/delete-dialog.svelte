a81d03763f1d584bcab600040251d2de0004959c
Cory Tyburski <corytyburski@gmail.com>
2023-09-08 21:03:28 -0400

    Setup test

---
+++ src/lib/delete-dialog.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import * as AlertDialog from '$lib/components/ui/alert-dialog';
+	import { Button } from '$lib/components/ui/button';
+	import { createEventDispatcher } from 'svelte';
+  import { itemIsSelected } from './store';
+
+	export let open = false;
+
+</script>
+
+<!-- Alert Dialog for deleting a menu item -->
+<AlertDialog.Root bind:open>
+	<AlertDialog.Content>
+		<AlertDialog.Header>
+			<AlertDialog.Title>
+				Are you sure you want to delete this item?
+			</AlertDialog.Title>
+			<AlertDialog.Description>
+				This action cannot be undone. This will permanently delete the item from or servers.
+			</AlertDialog.Description>
+		</AlertDialog.Header>
+		<AlertDialog.Footer>
+			<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+				<Button on:click={() => $itemIsSelected = false} variant="destructive">
+					Delete
+				</Button>
+		</AlertDialog.Footer>
+	</AlertDialog.Content>
+</AlertDialog.Root>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { Button } from "$lib/components/ui/button";
+  import DeleteDialog from "$lib/delete-dialog.svelte";
+  import { itemIsSelected } from "$lib/store";
+  import { fly } from "svelte/transition";
+
+  export let open = false;
+</script>
+
+<Button on:click={() => open = true}>Open Dialog</Button>
+ 
+{#if $itemIsSelected}
+  <div
+    in:fly={{ x: 400, duration: 250, opacity: 1 }}
+    out:fly={{ x: 400, duration: 250, opacity: 0 }}
+  >
+    <DeleteDialog bind:open />
+  </div>
+{/if}
